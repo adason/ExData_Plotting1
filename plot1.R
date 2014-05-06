6a66f8edd3853df168812ecada9b9e3ffae636ee
Adason <tdadason@gmail.com>
2014-05-06 18:15:39 -0400

    Add comment. Use with.

--- plot1.R
+++ plot1.R
@@ -1,12 +1,18 @@
 library(data.table)
-pc_data <- fread("household_power_consumption.txt", sep = ";",
-                 na.strings = "?")
+
+# Read the whole dataset into pc_data. 
+# Keep only the following two dates, "1/2/2007" and "2/2/2007".
+# Convert the Date column to IDate class and Time column to ITime class.
+pc_data <- fread("household_power_consumption.txt", sep = ";", na.strings = "?")
 pc_data <- subset(pc_data, Date %in% c("1/2/2007", "2/2/2007"))
 pc_data[,Date:=as.IDate(Date, format = "%d/%m/%Y")]
 pc_data[,Time:=as.ITime(Time, format = "%H:%M:%S")]
 
+# Plot and save to png file
 png("plot1.png", bg = "transparent")
-hist(pc_data$Global_active_power, breaks = 12,
-     col = "red", main =  "Global Active Power",
-     xlab = "Global Active Power (kilowatts)")
+with(pc_data, 
+     hist(Global_active_power, breaks = 12,
+          col = "red", main =  "Global Active Power",
+          xlab = "Global Active Power (kilowatts)")
+)
 dev.off()
